f7d825c395ae071bf91382b266444a662e0884be
Jin Linder <jinlinder@hotmail.com>
2022-03-21 12:23:16 +0100

    small changes

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var mongoose = require('mongoose');
-const user=require('./models/user')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,10 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-//test data
-
-
-//DATABASE
+//Connect DATABASE
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
@@ -37,11 +33,14 @@ mongoose.connect(process.env.MONGO_URL, {
     }
 })
 
-const saveUser = new user({
-    userName:"test1" ,
-      passWord:"test1" ,
-      babyAge:"test1",
-      location:"test1"
-})
-saveUser.save()
+
+
+// const saveUser = new user({
+//     userName:"test1" ,
+//       passWord:"test1" ,
+//       babyAge:"test1",
+//       location:"test1"
+// })
+// saveUser.save()
+
 module.exports = app;
