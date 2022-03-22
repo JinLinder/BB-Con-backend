d0eb060766ba538ce58023e61c096bd68913677d
Jin Linder <jinlinder@hotmail.com>
2022-03-22 13:25:12 +0100

    finished cancle function in login and signup

--- routes/login.js
+++ routes/login.js
@@ -7,8 +7,8 @@ router.post('/', function(req, res, next){
     user.find({username:req.body.username})
     .then(data => {
         if (data.length <= 1) {
-          console.log('user not found');
-          res.status(422).json({ message: `Användare inte hittad` });
+          console.log('Wrong username or password.');
+          res.status(422).json({ message: `Wrong username or password.` });
         }
       })  
     .catch((err)=> {
@@ -18,8 +18,8 @@ router.post('/', function(req, res, next){
     user.find({username:req.body.username, password:req.body.password})
     .then(data=>{
         if(data.length>=1) {
-            console.log('user match username and password');
-            res.status(201).json({ message: `Användare: ${req.body.username} inloggad` })
+            console.log(`User: ${req.body.username} loged in.`);
+            res.status(201).json({ message: `User: ${req.body.username} loged in.` })
         }
     })
     .catch((err)=> {
